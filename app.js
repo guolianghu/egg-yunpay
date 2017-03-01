a25549935aae17c550704e18a8ef0a2dbff2af1c
huguoliang <huguoliang@ehousechina.com>
2017-03-01 22:09:28 +0800

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by huguoliang on 2017/3/1.
+ */
+'use strict';
+
+const yunpayLoader = require('./lib/loader');
+
+module.exports = app => {
+    yunpayLoader(app);
+}
---
+++ config/config.default.js
@@ -0,0 +1,13 @@
+/**
+ * Created by huguoliang on 2017/3/1.
+ */
+'use strict';
+
+exports.yunpay = {
+    client: {
+        appid: '',
+        secret: '',
+        gate_url: '',
+    },
+    useAgent: false,
+}
