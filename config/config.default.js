7405c39c3a78906f0931c6308f32216199bb855f
伊长爽 <yichangshuang@yichangshuang-MacBook-Pro-15.local>
2017-03-02 15:08:44 +0800

    add 获取工作密钥

--- config/config.default.js
+++ config/config.default.js
@@ -5,9 +5,10 @@
 
 exports.yunpay = {
     client: {
-        partner: '',
-        secret: '',
-        gate_url: '',
+        partner: '100000',
+        secret: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDxAbF5OMdlj/NvI5tiohgj59N1R8MgJKJjddSCizDFJQmXU7L92iXDd+kflZpl8r7/GVswYKQGLhnm8kyrX+/daw+Ur5ianEjuXaMlJFnFZkaJGNWLx+odknLWYWnfUMEfvUy5kS2xNBXiPz9GjwNmgIj2HeeBnrIMsSmgE408mwIDAQAB',
+        gate_url: 'https://inte.ejupay.cn/gateway-outrpc/acquirer/interact',
+        terminalType: 'JSAPI',
     },
     useAgent: false,
 }
